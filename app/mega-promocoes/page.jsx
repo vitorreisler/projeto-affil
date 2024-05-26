553b9c765d28ad5c5f3156b33bd6c750f1800f9b
vitorreisler <vitorreisler@gmail.com>
2024-05-26 20:16:05 -0300

    alteracao layout

--- app/mega-promocoes/page.jsx
+++ app/mega-promocoes/page.jsx
@@ -9,34 +9,30 @@ const CategoriaMegaPromocoes = () => {
       <h1 className="text-6xl text-center p-3 bg-yellow-300 w-full inline">
         Mega Promoções  <GiFireworkRocket className="inline" />{" "}
       </h1>
-      <div className="grid grid-cols-1 md:grid-cols-3 flex-wrap md:gap-6 pt-5  md:py-4">
-        {/*gap-9 flex flex-wrap justify-center*/produtosMegaPromocoes.map((item) => {
+      <div className=" gap-9 flex flex-wrap justify-center py-4">
+        {produtosMegaPromocoes.map((item, i) => {
           return (
             <div
-              key={item.id}
-              className=" text-center hover:trasition mx-5 relative my-3 max-w-[208px] md:max-w-[13rem] rounded-2xl transition-all duration-500 hover:scale-105 border-2 shadow-[0_0_2px_#DC143C,inset_0_0_2px_#DC143C,0_0_5px_#DC143C,0_0_15px_#FF4500] "
+              key={i}
+              className="text-center  relative max-w-[12rem] border-2 shadow-[0_0_2px_#DC143C,inset_0_0_2px_#DC143C,0_0_5px_#DC143C,0_0_15px_#FF4500] rounded-2xl transition-all duration-500 hover:scale-105"
             >
-              <div className="block overflow-hidden max-h-[200px]">
+              <div className="block overflow-hidden object-cover">
                 <img
-                  className=" m-auto rounded-t-2xl object-cover"
+                  className=" m-auto rounded-t-2xl "
                   src={item.urlImg}
                   alt={item.alt}
                 />
               </div>
-              <div className="p-4 ">
-                <h4 className="text-base font-semibold text-gray-500 mb-2 capitalize transition-all duration-500 hover:text-gray-800 ">
+              <div className="p-4">
+                <h4 className="text-base font-semibold text-gray-900 mb-2 capitalize transition-all duration-500 ">
                   {item.nome}
                 </h4>
-                <p className={`text-sm font-normal ${item.descricao.length < 25 ? "h-20" : ""} hidden md:inline text-gray-500 transition-all duration-500 leading-5 mb-5 overflow-y-scroll scrollbar-hide`}>
-                  {item.descricao}
-                </p>
                 <p className="text-sm font-normal text-gray-500 transition-all duration-500 leading-5 mb-5">
-                  Categoria: {item.categoria}
+                  {item.descricao}
                 </p>
                 <a
                   href={item.linkDeCompra}
-                  target="_blank"
-                  className="bg-indigo-600 block shadow-sm rounded-md md:rounded-full py-2 px-5 text-xs text-white font-semibold"
+                  className="bg-indigo-600 shadow-sm rounded-full py-2 px-5 text-xs text-white font-semibold"
                 >
                   Ver produto...
                 </a>
