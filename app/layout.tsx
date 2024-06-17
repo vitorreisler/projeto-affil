624a0d1499e1c527c150c023e39aa4adf98f257d
vitorreisler <vitorreisler@gmail.com>
2024-06-17 13:51:42 -0300

    tentativaa pixel

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,9 +4,10 @@ import { siteConfig } from "@/config/site";
 import { fontSans } from "@/config/fonts";
 import { Providers } from "./providers";
 import { Navbar } from "@/components/navbar";
-import  Footer  from "@/components/footer";
-import { Link } from "@nextui-org/link";
+import Footer from "@/components/footer";
 import clsx from "clsx";
+import Head from "next/head";
+import Script from "next/script";
 
 export const metadata: Metadata = {
   title: {
@@ -33,6 +34,19 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en" suppressHydrationWarning>
+      <Head>
+        <Script
+          id="gtm-script"
+          strategy="afterInteractive"
+          dangerouslySetInnerHTML={{
+            __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+                      new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+                      j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+                      'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+                      })(window,document,'script','dataLayer','GTM-WXWJGDZ6');`,
+          }}
+        />
+      </Head>
       <head />
       <body
         className={clsx(
@@ -40,14 +54,20 @@ export default function RootLayout({
           fontSans.variable
         )}
       >
+        <noscript>
+          <iframe
+            src="https://www.googletagmanager.com/ns.html?id=GTM-WXWJGDZ6"
+            height="0"
+            width="0"
+            style={{ display: "none ", visibility: "hidden" }}
+          ></iframe>
+        </noscript>
         <Providers themeProps={{ attribute: "class", defaultTheme: "light" }}>
           <div className="relative flex flex-col h-screen">
             <Navbar />
-			
-            <main className=" ">
-              {children}
-            </main>
-              <Footer/>
+
+            <main className=" ">{children}</main>
+            <Footer />
           </div>
         </Providers>
       </body>
