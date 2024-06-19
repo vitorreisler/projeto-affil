093d97de7b8a38566b6ecb21775428071eff16da
vitorreisler <vitorreisler@gmail.com>
2024-06-19 17:10:39 -0300

    add produtos amamzon

--- produtos/produtosAmazon.js
+++ produtos/produtosAmazon.js
@@ -80,10 +80,20 @@ export const produtosAmazon = [
   {
     id: uuidv4(),
     urlImg:
+      "https://m.media-amazon.com/images/I/51zLcSJWCKL._AC_SL1200_.jpg",
+    nome: "Suporte para Notebook e Tablet em Alumínio Resistente Apoio Mesa em Metal Articulado Dobrável Ajustável",
+    descricao: "Suporte para Notebook e Tablet em Alumínio Resistente Apoio Mesa em Metal Articulado Dobrável Ajustável",
+    alt: "Suporte para Notebook e Tablet em Alumínio Resistente Apoio Mesa em Metal Articulado Dobrável Ajustável",
+    linkDeCompra: "https://amzn.to/3KN057b",
+    categoria:"Eletrônico"
+  },
+  {
+    id: uuidv4(),
+    urlImg:
       "https://m.media-amazon.com/images/I/81ZvbEt1aAL._AC_SL1500_.jpg",
-    nome: "Kindle 11ª Geração",
-    descricao: "Mais leve, com resolução de 300 ppi e o dobro de armazenamento - Cor Preta",
-    alt: "Kindle 11 geração",
+    nome: "Suporte para Notebook e Tablet em Alumínio Resistente Apoio Mesa em Metal Articulado Dobrável Ajustável",
+    descricao: "Suporte para Notebook e Tablet em Alumínio Resistente Apoio Mesa em Metal Articulado Dobrável Ajustável",
+    alt: "Suporte para Notebook e Tablet em Alumínio Resistente Apoio Mesa em Metal Articulado Dobrável Ajustável",
     linkDeCompra: "https://amzn.to/4ar5WJt",
     categoria:"Eletrônico"
   },
@@ -106,5 +116,105 @@ export const produtosAmazon = [
     alt: "Pai Rico, pai Pobre",
     linkDeCompra: "https://amzn.to/4bROSxp",
     categoria:"Livro"
+  },
+  {
+    id: uuidv4(),
+    urlImg:
+      "https://m.media-amazon.com/images/I/514bcom3x-L._AC_SL1000_.jpg",
+    nome: "Vonder Banqueta Plástica Dobrável Branca Altura 220 Mm",
+    descricao: "Vonder Banqueta Plástica Dobrável Branca Altura 220 Mm",
+    alt: "Vonder Banqueta Plástica Dobrável Branca Altura 220 Mm",
+    linkDeCompra: "https://amzn.to/4ckhsbi",
+    categoria:"Casa"
+  },
+  {
+    id: uuidv4(),
+    urlImg:
+      "https://m.media-amazon.com/images/I/71b3InIEJyL._AC_SL1200_.jpg",
+    nome: "Escrivaninha Trevalla Kuadra 150cm Preto Onix",
+    descricao: "Escrivaninha Trevalla Kuadra 150cm Preto Onix",
+    alt: "Escrivaninha Trevalla Kuadra 150cm Preto Onix",
+    linkDeCompra: "https://amzn.to/3xd1RLK",
+    categoria:"Casa"
+  },
+  {
+    id: uuidv4(),
+    urlImg:
+      "https://m.media-amazon.com/images/I/41sOAAI6e9L._AC_SL1200_.jpg",
+    nome: "cadeira de ergonomica escritorio",
+    descricao: "cadeira de ergonomica escritorio",
+    alt: "cadeira de ergonomica escritorio",
+    linkDeCompra: "https://amzn.to/45AQZEa",
+    categoria:"Casa"
+  },
+  {
+    id: uuidv4(),
+    urlImg:
+      "https://m.media-amazon.com/images/I/61VpeQ3B-JL._AC_SL1000_.jpg",
+    nome: "Cadeira de Escritório Secretária Base Cromada com Rodinha",
+    descricao: "Cadeira de Escritório Secretária Base Cromada com Rodinha",
+    alt: "Cadeira de Escritório Secretária Base Cromada com Rodinha",
+    linkDeCompra: "https://amzn.to/45ASySy",
+    categoria:"Casa"
+  },
+  {
+    id: uuidv4(),
+    urlImg:
+      "https://m.media-amazon.com/images/I/415FZBJSRQL._AC_SL1000_.jpg",
+    nome: "Cadeira de Refeição Portátil Smart Cosco - Cinza",
+    descricao: "Cadeira de Refeição Portátil Smart Cosco - Cinza",
+    alt: "Cadeira de Refeição Portátil Smart Cosco - Cinza",
+    linkDeCompra: "https://amzn.to/3KSxQEa",
+    categoria:"Casa"
+  },
+  {
+    id: uuidv4(),
+    urlImg:
+      "https://m.media-amazon.com/images/I/61VHqEuRgwL._AC_SL1000_.jpg",
+    nome: "Cosco Kids, Berço Portátil Happy, 0 a 15kg , Cinza",
+    descricao: "Cosco Kids, Berço Portátil Happy, 0 a 15kg , Cinza",
+    alt: "Cosco Kids, Berço Portátil Happy, 0 a 15kg , Cinza",
+    linkDeCompra: "https://amzn.to/3VtZ8W5",
+    categoria:"Casa"
+  },
+  {
+    id: uuidv4(),
+    urlImg:
+      "https://m.media-amazon.com/images/I/61e4dMDdxZL._AC_SL1200_.jpg",
+    nome: "Corda de Pular em PVC Vollo Vp1075",
+    descricao: "Corda de Pular em PVC Vollo Vp1075",
+    alt: "Corda de Pular em PVC Vollo Vp1075",
+    linkDeCompra: "https://amzn.to/3VP2R1W",
+    categoria:"Esporte"
+  },
+  {
+    id: uuidv4(),
+    urlImg:
+      "https://m.media-amazon.com/images/I/41CYGAvvnyL._AC_SL1000_.jpg",
+    nome: "MINI GALÃO 1875 mL",
+    descricao: "MINI GALÃO 1875 mL",
+    alt: "MINI GALÃO 1875 mL",
+    linkDeCompra: "https://amzn.to/3XqFYmI",
+    categoria:"Esporte"
+  },
+  {
+    id: uuidv4(),
+    urlImg:
+      "https://m.media-amazon.com/images/I/41fHfQbB81L._AC_SL1000_.jpg",
+    nome: "Mibee Compressor de ar portátil com inflador de pneu mini com bateria recarregável de 4000 mAh",
+    descricao: "Mibee Compressor de ar portátil com inflador de pneu mini com bateria recarregável de 4000 mAh",
+    alt: "Mibee Compressor de ar portátil com inflador de pneu mini com bateria recarregável de 4000 mAh",
+    linkDeCompra: "https://amzn.to/4b8Trmj",
+    categoria:"Esporte"
+  },
+  {
+    id: uuidv4(),
+    urlImg:
+      "https://m.media-amazon.com/images/I/51baqU9RjNL._AC_SL1500_.jpg",
+    nome: "Speedo Flat Cap , Unissex",
+    descricao: "Speedo Flat Cap , Unissex",
+    alt: "Speedo Flat Cap , Unissex",
+    linkDeCompra: "https://amzn.to/3XuIw32",
+    categoria:"Esporte"
   },
 ];
