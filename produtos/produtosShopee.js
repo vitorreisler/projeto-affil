21843639f003f177a9bde62fcd86952a22063d2e
vitorreisler <vitorreisler@gmail.com>
2024-06-19 15:15:11 -0300

    add produto shopee

--- produtos/produtosShopee.js
+++ produtos/produtosShopee.js
@@ -3,6 +3,16 @@ export const produtosShopee = [
   {
     id: uuidv4(),
     urlImg:
+      "https://down-br.img.susercontent.com/file/sg-11134201-7rcdr-ltr6rhko1t97c6",
+    nome: "Pote de Vidro Patê 156ml Copo Pra Vela Doces Geleias Patês ",
+    descricao: "Pote de Vidro Patê 156ml Copo Pra Vela Doces Geleias Patês ",
+    alt: "Pote de Vidro Patê 156ml Copo Pra Vela Doces Geleias Patês ",
+    linkDeCompra: "https://s.shopee.com.br/5fTjY8fB4z",
+    categoria: "",
+  },
+  {
+    id: uuidv4(),
+    urlImg:
       "https://down-br.img.susercontent.com/file/br-11134207-7r98o-lvakv57eyndk8c",
     nome: "Kit 3 Garrafas ",
     descricao: "2Litros, 900ml e 300ml Frase Motivacional",
@@ -24,11 +34,34 @@ export const produtosShopee = [
   {
     id: uuidv4(),
     urlImg:
-      "https://down-tx-br.img.susercontent.com/br-11134207-7r98o-lui6ym94kvol82.webp",
-    nome: "Cortador Multifuncional ",
-    descricao: "Cortador Multifuncional Fatiador De Verduras/frutas/legumes",
-    alt: "Cortador Multifuncional ",
-    linkDeCompra: "https://shope.ee/10hG8HPLOL",
+      "https://down-cvs-br.img.susercontent.com/br-11134207-7qukw-li39v6ya14zj14.webp",
+    nome: " Régua Filtro de Linha",
+    descricao:
+      "Régua Filtro de Linha com 7 Tomadas Extensão Elétrica 1 Universal 4 USB Bivolt 10A 110v/220v",
+    alt: " Régua Filtro de Linha",
+    linkDeCompra: "https://s.shopee.com.br/50DOwMcMsm",
+    categoria: "",
+  },
+  {
+    id: uuidv4(),
+    urlImg:
+      "https://down-cvs-br.img.susercontent.com/br-11134207-7qukw-lh52iekd6ivfbc.webp",
+    nome: "Mini Projetor",
+    descricao:
+      "Mini Projetor Portátil Curta Distancia Led 1080p 600 Lúmens Hdmi YG300 Envio Imediato",
+    alt: "Mini Projetor",
+    linkDeCompra: "https://s.shopee.com.br/3L5AxNcV34",
+    categoria: "",
+  },
+  {
+    id: uuidv4(),
+    urlImg:
+      "https://down-tx-br.img.susercontent.com/br-11134207-7r98o-loq5fq9eqshe7b.webp",
+    nome: "Kit 3 Câmeras de segurança",
+    descricao:
+      "Kit 3 Câmeras de segurança a Prova D'água sem fio gira 360 com app Yousee full HD visão notturna",
+    alt: "Kit 3 Câmeras de segurança",
+    linkDeCompra: "https://shope.ee/2LCdjkPY5j",
     categoria: "",
   },
   {
@@ -55,12 +88,43 @@ export const produtosShopee = [
   {
     id: uuidv4(),
     urlImg:
-      "https://down-tx-br.img.susercontent.com/br-11134207-7r98o-loq5fq9eqshe7b.webp",
-    nome: "Kit 3 Câmeras de segurança",
+      "https://down-tx-br.img.susercontent.com/br-11134207-7r98o-lui6ym94kvol82.webp",
+    nome: "Cortador Multifuncional ",
+    descricao: "Cortador Multifuncional Fatiador De Verduras/frutas/legumes",
+    alt: "Cortador Multifuncional ",
+    linkDeCompra: "https://shope.ee/10hG8HPLOL",
+    categoria: "",
+  },
+  {
+    id: uuidv4(),
+    urlImg:
+      "https://down-br.img.susercontent.com/file/66b5eb1ca084fb400497205679a0dbb8",
+    nome: "Mini Processador Elétrico",
     descricao:
-      "Kit 3 Câmeras de segurança a Prova D'água sem fio gira 360 com app Yousee full HD visão notturna",
-    alt: "Kit 3 Câmeras de segurança",
-    linkDeCompra: "https://shope.ee/2LCdjkPY5j",
+      "Moedor Carne Vegetais Alho Fruta Amendoim Recarregável USB 250ml",
+    alt: "Mini Processador Elétrico",
+    linkDeCompra: "https://s.shopee.com.br/8pQdXwSKt3",
+    categoria: "",
+  },
+  {
+    id: uuidv4(),
+    urlImg:
+      "https://down-br.img.susercontent.com/file/br-11134201-22120-2sg3yak8i7kvfc",
+    nome: "Cortador Elétrico De Legumes 4 Em 1",
+    descricao:
+      "Sem Fio Com Carregamento USB Triturador Durável Ferramenta De Cozinha",
+    alt: "Cortador Elétrico De Legumes 4 Em 1",
+    linkDeCompra: "https://s.shopee.com.br/1B1CQ1M8ws",
+    categoria: "",
+  },
+  {
+    id: uuidv4(),
+    urlImg:
+      "https://down-br.img.susercontent.com/file/br-11134207-7r98o-lqm2zomjqasn23",
+    nome: "Dispenser Automático De Ovos 2 Andares ",
+    descricao: "Dispenser Automático De Ovos 2 Andares ",
+    alt: "Dispenser Automático De Ovos 2 Andares ",
+    linkDeCompra: "https://s.shopee.com.br/8f7DNYgUr2",
     categoria: "",
   },
   {
@@ -74,17 +138,7 @@ export const produtosShopee = [
     linkDeCompra: "https://s.shopee.com.br/2fpU8fTb7M",
     categoria: "",
   },
-  {
-    id: uuidv4(),
-    urlImg:
-      "https://down-tx-br.img.susercontent.com/sg-11134201-7qvfc-lhko9hdo8tf899.webp",
-    nome: "5PCS Meias",
-    descricao:
-      "5PCS Meias De Corrida casual Com Algodão Combinado (Pronto Para Enviar)",
-    alt: "5PCS Meias",
-    linkDeCompra: "https://shope.ee/4ptyikNSl8",
-    categoria: "",
-  },
+
   {
     id: uuidv4(),
     urlImg:
@@ -120,39 +174,6 @@ export const produtosShopee = [
   {
     id: uuidv4(),
     urlImg:
-      "https://down-tx-br.img.susercontent.com/br-11134207-7r98o-lr3r17olxctg23.webp",
-    nome: " Calças Jogger Masculina",
-    descricao:
-      "Calças Jogger Masculina com punho Premium qualidade moda homem Estiloso",
-    alt: " Calças Jogger Masculina",
-    linkDeCompra: "https://shope.ee/1LK6Yofj50",
-    categoria: "",
-  },
-  {
-    id: uuidv4(),
-    urlImg:
-      "https://down-cvs-br.img.susercontent.com/br-11134207-7qukw-li39v6ya14zj14.webp",
-    nome: " Régua Filtro de Linha",
-    descricao:
-      "Régua Filtro de Linha com 7 Tomadas Extensão Elétrica 1 Universal 4 USB Bivolt 10A 110v/220v",
-    alt: " Calças Jogger Masculina",
-    linkDeCompra: "https://s.shopee.com.br/50DOwMcMsm",
-    categoria: "",
-  },
-  {
-    id: uuidv4(),
-    urlImg:
-      "https://down-cvs-br.img.susercontent.com/br-11134207-7qukw-lh52iekd6ivfbc.webp",
-    nome: "Mini Projetor",
-    descricao:
-      "Mini Projetor Portátil Curta Distancia Led 1080p 600 Lúmens Hdmi YG300 Envio Imediato",
-    alt: "Mini Projetor",
-    linkDeCompra: "https://s.shopee.com.br/3L5AxNcV34",
-    categoria: "",
-  },
-  {
-    id: uuidv4(),
-    urlImg:
       "https://down-cvs-br.img.susercontent.com/br-11134207-7r98o-lr7hn7b9mnnf46.webp",
     nome: "Edredom Casal Dupla Face",
     descricao:
@@ -164,11 +185,53 @@ export const produtosShopee = [
   {
     id: uuidv4(),
     urlImg:
+      "https://down-tx-br.img.susercontent.com/br-11134207-7r98o-lr3r17olxctg23.webp",
+    nome: " Calças Jogger Masculina",
+    descricao:
+      "Calças Jogger Masculina com punho Premium qualidade moda homem Estiloso",
+    alt: " Calças Jogger Masculina",
+    linkDeCompra: "https://shope.ee/1LK6Yofj50",
+    categoria: "",
+  },
+  {
+    id: uuidv4(),
+    urlImg:
       "https://down-cvs-br.img.susercontent.com/ec24079ddfc4e4addd0877255feae687.webp",
     nome: "Bermuda Cargo Masculina",
     descricao: "Bermuda Cargo Masculina Folgada De Verão new",
     alt: "Bermuda Cargo Masculina",
     linkDeCompra: "https://shope.ee/8KTqvB3uPR",
+    categoria: "",
+  },
+  {
+    id: uuidv4(),
+    urlImg:
+      "https://down-br.img.susercontent.com/file/sg-11134201-7rd4s-lwh41he3b04l31",
+    nome: "Varal De Chão  Dobrável Portátil",
+    descricao: "Varal De Chão Roupa Calcinha Grade 3 Andares Dobrável Portátil",
+    alt: "Varal De Chão  Dobrável Portátil",
+    linkDeCompra: "https://s.shopee.com.br/3L5h0I2Qka",
+    categoria: "",
+  },
+  {
+    id: uuidv4(),
+    urlImg:
+      "https://down-tx-br.img.susercontent.com/sg-11134201-7qvfc-lhko9hdo8tf899.webp",
+    nome: "5PCS Meias",
+    descricao:
+      "5PCS Meias De Corrida casual Com Algodão Combinado (Pronto Para Enviar)",
+    alt: "5PCS Meias",
+    linkDeCompra: "https://shope.ee/4ptyikNSl8",
+    categoria: "",
+  },
+  {
+    id: uuidv4(),
+    urlImg:
+      "https://down-br.img.susercontent.com/file/br-11134201-23020-uk52szc2w7mv9d",
+    nome: "Varal Em Aço Dobrável + 20 Metros Cabo Aço Encapado",
+    descricao: "Varal Em Aço Dobrável + 20 Metros Cabo Aço Encapado",
+    alt: "Varal Em Aço Dobrável + 20 Metros Cabo Aço Encapado",
+    linkDeCompra: "https://s.shopee.com.br/4puUnI8GAM",
     categoria: "",
   },
   {
@@ -186,57 +249,20 @@ export const produtosShopee = [
     urlImg:
       "https://down-br.img.susercontent.com/file/sg-11134201-7rd5e-lwau466lholp7f",
     nome: "Liquidificador 3 velocidades 550W Turbo Power - L-99-FB - Mondial",
-    descricao: "Liquidificador 3 velocidades 550W Turbo Power - L-99-FB - Mondial",
+    descricao:
+      "Liquidificador 3 velocidades 550W Turbo Power - L-99-FB - Mondial",
     alt: "Liquidificador 3 velocidades 550W Turbo Power - L-99-FB - Mondial",
     linkDeCompra: "https://s.shopee.com.br/pF1UIome",
     categoria: "",
   },
-  {
-    id: uuidv4(),
-    urlImg:
-      "https://down-br.img.susercontent.com/file/66b5eb1ca084fb400497205679a0dbb8",
-    nome: "Mini Processador Elétrico",
-    descricao: "Moedor Carne Vegetais Alho Fruta Amendoim Recarregável USB 250ml",
-    alt: "Mini Processador Elétrico",
-    linkDeCompra: "https://s.shopee.com.br/8pQdXwSKt3",
-    categoria: "",
-  },
-  {
-    id: uuidv4(),
-    urlImg:
-      "https://down-br.img.susercontent.com/file/br-11134201-22120-2sg3yak8i7kvfc",
-    nome: "Cortador Elétrico De Legumes 4 Em 1",
-    descricao: "Sem Fio Com Carregamento USB Triturador Durável Ferramenta De Cozinha",
-    alt: "Cortador Elétrico De Legumes 4 Em 1",
-    linkDeCompra: "https://s.shopee.com.br/1B1CQ1M8ws",
-    categoria: "",
-  },
-  {
-    id: uuidv4(),
-    urlImg:
-      "https://down-br.img.susercontent.com/file/sg-11134201-7rd4s-lwh41he3b04l31",
-    nome: "Varal De Chão  Dobrável Portátil",
-    descricao: "Varal De Chão Roupa Calcinha Grade 3 Andares Dobrável Portátil",
-    alt: "Varal De Chão  Dobrável Portátil",
-    linkDeCompra: "https://s.shopee.com.br/3L5h0I2Qka",
-    categoria: "",
-  },
-  {
-    id: uuidv4(),
-    urlImg:
-      "https://down-br.img.susercontent.com/file/br-11134201-23020-uk52szc2w7mv9d",
-    nome: "Varal Em Aço Dobrável + 20 Metros Cabo Aço Encapado",
-    descricao: "Varal Em Aço Dobrável + 20 Metros Cabo Aço Encapado",
-    alt: "Varal Em Aço Dobrável + 20 Metros Cabo Aço Encapado",
-    linkDeCompra: "https://s.shopee.com.br/4puUnI8GAM",
-    categoria: "",
-  },
+  
   {
     id: uuidv4(),
     urlImg:
       "https://down-br.img.susercontent.com/file/br-11134207-7r98r-llkq0yf5whqre5",
     nome: "Mini Aspirador Portátil Automotivo Potente Recarregável Sem Fio",
-    descricao: "Mini Aspirador Portátil Automotivo Potente Recarregável Sem Fio",
+    descricao:
+      "Mini Aspirador Portátil Automotivo Potente Recarregável Sem Fio",
     alt: "Mini Aspirador Portátil Automotivo Potente Recarregável Sem Fio",
     linkDeCompra: "https://s.shopee.com.br/7AIPa3Mn8E",
     categoria: "",
@@ -259,16 +285,6 @@ export const produtosShopee = [
     descricao: "Escova Eletrica de Limpeza 5 em 1 Sem Fio",
     alt: "Escova Eletrica de Limpeza 5 em 1 Sem Fio",
     linkDeCompra: "https://s.shopee.com.br/8KUMyhEYgJ",
-    categoria: "",
-  },
-  {
-    id: uuidv4(),
-    urlImg:
-      "https://down-br.img.susercontent.com/file/br-11134207-7r98o-lqm2zomjqasn23",
-    nome: "Dispenser Automático De Ovos 2 Andares ",
-    descricao: "Dispenser Automático De Ovos 2 Andares ",
-    alt: "Dispenser Automático De Ovos 2 Andares ",
-    linkDeCompra: "https://s.shopee.com.br/8f7DNYgUr2",
     categoria: "",
   },
 ];
